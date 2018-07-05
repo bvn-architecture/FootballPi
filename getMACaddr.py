45804400c45a0dfda6a7c20ff4305e9f4c12990b
AidenRay <aidenunsw@outlook.com>
2018-07-05 18:45:41 +1000

    added MAC address script

---
+++ getMACaddr.py
@@ -0,0 +1,33 @@
+import sys
+import csv
+
+def getEthName():
+    # Get name of the Ethernet interface
+    try:
+        for root, dirs,files in os.walk('/sys/class/net'):
+            for dir in dirs:
+                if dir[:3]=='enx' or dir[:3]=='eth':
+                    Sinterface=dir   
+    except:
+        interface="None"
+    return interface
+
+
+def getMAC(interface='eth0'):
+    # Return the MAC address of the specified interface
+    try:
+        str1 = open('/sys/class/net/%s/address' %interface).read()
+    except:
+        str1 = "00:00:00:00:00:00"
+
+    return str1[0:17]
+
+if __name__ == "__main__":
+    ethName = getEthName()
+    ethMAC = getMAC(ethName)
+    name = sys.argv[1]
+    row = [name, ethMAC]
+    with open('MAC adresses.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(row)
+
