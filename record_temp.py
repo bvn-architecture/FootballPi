3c88b2d1b385fd242062e86d3d09a86d55cc6341
aidenray <aidenray@github.com>
2018-07-30 04:01:44 +0000

    dynamically grabs sensor ID

--- record_temp.py
+++ record_temp.py
@@ -1,8 +1,10 @@
 from w1thermsensor import W1ThermSensor
 
 for sensor in W1ThermSensor.get_available_sensors():
-    print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
+    print("Sensor {} {} has temperature {}".format(sensor, sensor.id, sensor.get_temperature()))
+    id = sensor.id
 
-
-sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "00000588806a")
+sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, id)
 temperature_in_celsius = sensor.get_temperature()
+print(temperature_in_celsius)
+
